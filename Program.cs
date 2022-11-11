f1303263f5b2235a48f5895827c9da3105f7ea19
Alex <automex74@gmail.com>
2022-11-11 22:38:15 +0500

    add task_62

--- Program.cs
+++ Program.cs
@@ -186,5 +186,40 @@ void PrintArray3D(int[,,] Array)
     }
 }
 
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Console.WriteLine("--------------Задача №62.--------------");
+int m = 4;
+int[,] spiralArray = new int[m, m];
+int t = 1; int i = 0; int j = 0;
 
+while (t <= spiralArray.GetLength(0) * spiralArray.GetLength(1))
+{
+    spiralArray[i, j] = t;
+    t++;
+    if (i <= j + 1 && i + j < spiralArray.GetLongLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= spiralArray.GetLongLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > spiralArray.GetLongLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+PrintArray1(spiralArray);
+
+void PrintArray1(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+            Console.Write($" {array[i,j]} ");
+
+            else Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
