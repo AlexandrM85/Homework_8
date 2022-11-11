96400c098324288472105e5808bda6fead9df990
Alex <automex74@gmail.com>
2022-11-11 17:04:45 +0500

    add task_58

--- Program.cs
+++ Program.cs
@@ -85,3 +85,44 @@ int StringMinSum(int[,] arr)
     }
     return result;
 }
+
+// Задача 58: Задайте две квадратные матрицы. Напишите программу, которая будет находить
+// произведение двух матриц.
+
+Console.WriteLine("Введите колличество строк 1 массива");
+int rowsA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите колличество столбцов 1 массива");
+int columnsA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите колличество строк 2 массива");
+int rowsB = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите колличество столбцов 2 массива");
+int columnsB = int.Parse(Console.ReadLine()!);
+if (columnsA != rowsB)
+{
+    Console.WriteLine("Данные матрицы нельзя умножать.");
+    return;
+}
+
+int[,] A = GetArray(rowsA, columnsA, 0, 10);
+int[,] B = GetArray(rowsB, columnsB, 0, 10);
+PrintArray(A);
+Console.WriteLine("--------");
+PrintArray(B);
+Console.WriteLine("--------");
+PrintArray(MultiplicationMatrix(A,B));
+
+int[,] MultiplicationMatrix(int[,] array1, int[,] array2)
+{
+    int[,] array3 = new int[array1.GetLength(0), array2.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                array3[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return array3;
+}
